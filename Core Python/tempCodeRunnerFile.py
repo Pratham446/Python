9d5ba8dabaec666ebc37ffd320979e5edadc8ead
Pratham Darji <161615270+Pratham446@users.noreply.github.com>
2025-07-10 18:21:12 +0530

    Delete Core Python/tempCodeRunnerFile.py

--- Core Python/tempCodeRunnerFile.py
+++
@@ -1,9 +0,0 @@
-
-# cube=lambda x:x**3
-# print(cube(3))
-
-# # *args lets you pass multiple values to a function without listing them one by one in the definition.
-# def add_numbers(*args):
-#     return sum(args)
-
-# print(add_numbers(1, 2, 3, 4))
